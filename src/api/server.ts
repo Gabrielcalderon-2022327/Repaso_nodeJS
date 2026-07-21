3d1164a7f5b4ebb7bcd7c65fc76b168a0acb9d4f
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-21 09:25:07 -0600

    fix: Se corrigio un error de puerto

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,24 +1,23 @@
-import { createServer} from "http";
+import { createServer } from "http";
 import { router } from "./router";
 const PORT = 3000;
 
 
-export async function iniciarServer(): Promise<void> {
-    const server = createServer(async (req, res) => {
-        try {
-            await router(req, res);
-        } catch (error) {
-            console.error('Error interno:', error);
-            res.writeHead(500, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify({ error: 'Error interno del servidor' }));
-        }
-    });
 
-    server.listen(PORT, () => {
-        console.log("\n--------------------------------");
-        console.log("SERVER INCIADO");
-        console.log(`PUERTO: ${PORT}`);
-        console.log("URL: http://localhost:" + PORT);
-        console.log("--------------------------------");
-    });
-}
+const server = createServer(async (req, res) => {
+    try {
+        await router(req, res);
+    } catch (error) {
+        console.error('Error interno:', error);
+        res.writeHead(500, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ error: 'Error interno del servidor' }));
+    }
+});
+
+server.listen(PORT, () => {
+    console.log("\n--------------------------------");
+    console.log("SERVER INCIADO");
+    console.log(`PUERTO: ${PORT}`);
+    console.log("URL: http://localhost:" + PORT);
+    console.log("--------------------------------");
+});
