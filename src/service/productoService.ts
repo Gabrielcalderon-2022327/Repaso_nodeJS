6a48ef1901957a4a1d6a5f84b3e621e1d499d0f6
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-07-21 08:59:02 -0600

    feat: Se agrego service de producto

---
+++ src/service/productoService.ts
@@ -0,0 +1,50 @@
+import { Producto } from "../models/producto";
+import { leerProductos, escribirProductos } from "../data/productoRepository";
+
+
+export async function listarProductos(): Promise<Producto[]> {
+    const productos: Producto[] = await leerProductos();
+    return productos;
+}
+
+export async function agregarProducto(producto: Producto): Promise<void> {
+    const productos: Producto[] = await leerProductos();
+    producto.id = productos.length + 1;
+    productos.push(producto);
+    await escribirProductos(productos);
+}
+
+export async function buscarProducto(id: number): Promise<Producto | null> {
+    const productos: Producto[] = await leerProductos();
+    return productos.find(p => p.id === id) || null;
+}
+
+export async function eliminarProducto(id: number): Promise<boolean> {
+    const productos: Producto[] = await leerProductos();
+    if (id <= 0) return false;
+
+    const index = productos.findIndex(p => p.id === id);
+
+    if (index === -1){
+        return false;
+    }
+
+    productos.splice(index,1);
+    await escribirProductos(productos);
+    return true;
+}
+
+export async function editarProducto(id: number, producto: Producto): Promise<boolean> {
+    const productos: Producto[] = await leerProductos();
+    if (id <= 0) return false;
+
+    const index = productos.findIndex(p => p.id === id);
+
+    if (index === -1){
+        return false;
+    }
+
+    productos[index] = {...productos[index], ...producto};
+    await escribirProductos(productos);
+    return true;
+}
